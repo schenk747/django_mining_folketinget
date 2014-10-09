9ec74eb198a7458abfbbc4529bc5054b45be61c1
schenk <christopher.schenk@gmail.com>
2014-10-09 18:36:04 +0200

    changed urls.py

--- mining_folketinget/mining_folketinget/urls.py
+++ mining_folketinget/mining_folketinget/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mining_folketinget.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+		url(r'^query_folketinget/', include('query_folketinget.urls', namespace='query_folketinget')),
     url(r'^admin/', include(admin.site.urls)),
 )
